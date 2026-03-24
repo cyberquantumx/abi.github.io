76b24241e256cf273f1d103f71456b9b621bc731
Илья <33177966+cyberquantumx@users.noreply.github.com>
2026-03-24 12:50:12 +0400

    Delete framework directory
    Signed-off-by: Илья <33177966+cyberquantumx@users.noreply.github.com>

--- framework/resources/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    AOS.init();
-    $('.heart').click(function() {
-        window.open('https://www.tbank.ru/cf/5yf7xr6MoaH', '_blank');
-    });
-    $('#tgBtn').click(function() {
-        window.open('https://t.me/cyberboyx', '_blank');
-    });
-});
